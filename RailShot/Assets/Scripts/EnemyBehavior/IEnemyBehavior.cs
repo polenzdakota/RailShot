2d4aad347d50465fcec3f3f14e2783bacbb839ff
Dakota Polenz <polenzdakota@yahoo.com>
2017-07-17 14:00:19 -0700

    initial project

---
+++ RailShot/Assets/Scripts/EnemyBehavior/IEnemyBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+*  Interface for enemy types
+**/
+public interface IEnemyBehavior {
+	
+	void Movement();
+	
+	void Trigger();
+	
+	void OnDeath();
+	
+	void OnSpawn();
+}
