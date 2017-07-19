d4bdd1ac885bf633d65d863a8520b870dac26ade
Dakota Polenz <polenzdakota@yahoo.com>
2017-07-18 17:06:21 -0700

    Stubs for state control

--- RailShot/Assets/Scripts/StateControl.cs
+++ RailShot/Assets/Scripts/StateControl.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+*  Class for updating the current state of game. 
+*  Mostly called on by UI elements
+**/
 public class StateControl : MonoBehaviour {
 
 	// Use this for initialization
@@ -9,8 +13,28 @@ public class StateControl : MonoBehaviour {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	//On press Start
+	public void PressStart() {
+		//TODO
+	}
+	
+	//On restart
+	public void PressRestart() {
+		//TODO
+	}
+	
+	//On press option
+	public void PressOption() {
+		//TODO
+	}
+	
+	//On press other
+	public void PressOther() {
+		//TODO
+	}
+	
+	//On press pause
+	public void Pause() {
+		//TODO
 	}
 }
